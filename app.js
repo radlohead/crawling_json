88b96b3fdc40b08495e472a3e6304c14dae606f8
minho <radlohead@naver.com>
2017-10-07 13:24:17 +0900

    url분리

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let cheerio = require('cheerio');
+let urls = require('./urls');
 
 let crawling = function (url) {
     let json = [];
@@ -93,8 +94,5 @@ let crawling = function (url) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const url = ["http://franchise.ftc.go.kr/user/extra/main/62/firMst/view/jsp/LayOutPage.do?dataIdx=39218&spage=1&srow=10&column=t_nm&search=%EB%86%80%EB%B6%80",
-        "http://franchise.ftc.go.kr/user/extra/main/62/firMst/view/jsp/LayOutPage.do?dataIdx=48574&spage=1&srow=10&column=&search="];
-
-    crawling(url);
+    crawling(urls.url);
 });
---
+++ urls.js
@@ -0,0 +1,4 @@
+export const url = [
+    "http://franchise.ftc.go.kr/user/extra/main/62/firMst/view/jsp/LayOutPage.do?dataIdx=39218&spage=1&srow=10&column=t_nm&search=%EB%86%80%EB%B6%80",
+    "http://franchise.ftc.go.kr/user/extra/main/62/firMst/view/jsp/LayOutPage.do?dataIdx=48574&spage=1&srow=10&column=&search="
+];
